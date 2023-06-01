aad7ffb86a3f5d1f07ab2f6f0576cf12280c66d5
MUTHUKRISHNAN S <77878932+MuthuKrishnan27@users.noreply.github.com>
2023-06-01 19:49:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+		let ul = document.getElementsByTagName("ul")[0];
+        // console.log(ul.children);
+        // console.log(ul.children.length);
+        
+        for(let i=0;i<ul.children.length;i++){
+            // console.log(ul.children[i]);
+            if(ul.children[i].id === "level"){
+                let index = i+1;
+                alert("The level of the element is: "+index);
+            }
